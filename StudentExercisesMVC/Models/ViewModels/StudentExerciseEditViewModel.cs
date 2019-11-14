f8422447c20ed76028637faa3b88861b2963b6b4
Allison Patton <allison.tpttn@gmail.com>
2019-11-14 10:53:04 -0600

    started working on SEpart2. Created seEditViewModel

---
+++ StudentExercisesMVC/Models/ViewModels/StudentExerciseEditViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace StudentExercisesMVC.Models.ViewModels
+{
+    public class StudentExerciseEditViewModel
+    {
+        public Student Student { get; set; }
+        //public List<Exercise> Exercises { get; set; } = new List<Exercise>();
+        public MultiSelectList Exercises { get; set; }
+
+        //public List<SelectListItem> CohortOptions
+        //{
+        //    get
+        //    {
+        //        if (Cohorts == null) return null;
+
+        //        return Cohorts
+        //            .Select(c => new SelectListItem(c.Name, c.Id.ToString()))
+        //            .ToList();
+        //    }
+        //}
+    }
+}
